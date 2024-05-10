a65b41943673e41bcbacae912ce8fe5d03cf22b3
C42xe2 <erikdonath2006@gmail.com>
2024-05-10 16:48:02 +0200

    New Chunk System v1
    Seiten im Chunk werden jetzt nicht mehr gerendert.

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,71 +1,60 @@
-using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour {
     [SerializeField] private Material worldMaterial;
 
-    private MeshFilter m_filter;
-    private MeshRenderer m_renderer;
+    private MeshFilter filter;
 
-    private List<Vector3> vertices = new List<Vector3>();
-    private List<Vector2> uvs = new List<Vector2>();
-    private List<int> indeces = new List<int>();
-    private int triangleIndex = 0;
+    VoxelMesh voxelMesh = new VoxelMesh();
+    bool[,,] blocks = new bool[16, 16, 16];
 
     private void Start() {
-        m_filter = this.AddComponent<MeshFilter>();
-        m_renderer = this.AddComponent<MeshRenderer>();
-        m_renderer.sharedMaterial = worldMaterial;
+        filter = this.AddComponent<MeshFilter>();
+        MeshRenderer renderer = this.AddComponent<MeshRenderer>();
+        renderer.sharedMaterial = worldMaterial;
+
+        for(int i = 0; i < blocks.GetLength(0); i++){
+            for (int j = 0; j < blocks.GetLength(1); j++){
+                for (int k = 0; k < blocks.GetLength(2); k++) {
+                    blocks[i, j, k] = (i + j + k) % 2 == 0;
+                }
+            }
+        }
 
         GenerateMesh();
     }
 
-    private void GenerateMesh() {
-        ClearMesh();
+    private bool IsSolid(Vector3Int pos) {
+        if (pos.x < 0 || pos.x >= blocks.GetLength(0) ||
+            pos.y < 0 || pos.y >= blocks.GetLength(1) ||
+            pos.z < 0 || pos.z >= blocks.GetLength(2)
+        ) return false;
+        return blocks[pos.x, pos.y, pos.z];
+    }
 
-        for (int a = 0; a < 5; a++) {
-            for (int b = 0; b < 5; b++) {
-                for (int c = 0; c < 5; c++) {
-                    AddVoxel(new Vector3(a, b, c));
+    private void GenerateMesh() {
+        voxelMesh.ClearMesh();
+
+        for (int i = 0; i < blocks.GetLength(0); i++) {
+            for (int j = 0; j < blocks.GetLength(1); j++) {
+                for (int k = 0; k < blocks.GetLength(2); k++) {
+                    if (IsSolid(new Vector3Int(i, j, k))) {
+                        AddVoxel(new Vector3Int(i, j, k));
+                    }
                 }
             }
         }
-        SendMesh();
+
+        filter.mesh = voxelMesh.GenerateMesh();
     }
 
-    private void AddVoxel(Vector3 position) {
+    private void AddVoxel(Vector3Int position) {
         for(int j = 0; j < 6; j++) {
-            for(int i = 0; i < 6; i++) {
-                int index = VoxelData.voxelIndeces[j, i];
-                vertices.Add(position + VoxelData.voxelVertices[index]);
-                uvs.Add(VoxelData.voxelUvs[i]);
-
-                indeces.Add(i + triangleIndex);
+            if (!IsSolid(position + VoxelData.inDirection[j])) {
+                voxelMesh.AddFace(position, (FaceDirection)j, new Vector4(0, 0, 1, 1));
             }
-            triangleIndex += 6;
         }
     }
 
-    private void ClearMesh() {
-        vertices.Clear();
-        uvs.Clear();
-        indeces.Clear();
-        triangleIndex = 0;
-    }
-
-
-    private void SendMesh() {
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = indeces.ToArray();
-        mesh.uv = uvs.ToArray();
-        
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-        mesh.RecalculateTangents();
-
-        mesh.name = "Chunk Mesh - " + transform.position.x + ", " + transform.position.y;
-        m_filter.mesh = mesh;
-    }
 }
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -1,32 +1,126 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class VoxelData {
-    public static readonly Vector3[] voxelVertices = new Vector3[8] {
-        new Vector3(0.0f, 0.0f, 0.0f), // 0
-        new Vector3(1.0f, 0.0f, 0.0f), // 1
-        new Vector3(1.0f, 1.0f, 0.0f), // 2
-        new Vector3(0.0f, 1.0f, 0.0f), // 3
-        new Vector3(0.0f, 0.0f, 1.0f), // 4
-        new Vector3(1.0f, 0.0f, 1.0f), // 5
-        new Vector3(1.0f, 1.0f, 1.0f), // 6
-        new Vector3(0.0f, 1.0f, 1.0f), // 7
+    // X+ => East
+    // X- => West
+    // Z+ => North
+    // Z- => South
+    // Y+ => Up
+    // Y- => Down
+
+    public static readonly Vector3[] vertices = new Vector3[8] {
+        new Vector3(0, 0, 0), // 0
+        new Vector3(1, 0, 0), // 1
+        new Vector3(0, 1, 0), // 2
+        new Vector3(1, 1, 0), // 3
+        new Vector3(0, 0, 1), // 4
+        new Vector3(1, 0, 1), // 5
+        new Vector3(0, 1, 1), // 6
+        new Vector3(1, 1, 1), // 7
     };
 
-    public static readonly int[,] voxelIndeces = new int[6, 6] {
-        { 0, 3, 1, 1, 3, 2 }, // Back
-        { 5, 6, 4, 4, 6, 7 }, // Front
-        { 3, 7, 2, 2, 7, 6 }, // Top
-        { 1, 5, 0, 0, 5, 4 }, // Bottom
-        { 4, 7, 0, 0, 7, 3 }, // Left
-        { 1, 2, 5, 5, 2, 6 }, // Right
+    public static readonly int[,] indeces = new int[6, 4] {
+        { 5, 7, 4, 6 }, // North
+        { 0, 2, 1, 3 }, // South
+
+        { 4, 6, 0, 2 }, // West
+        { 1, 3, 5, 7 }, // East
+
+        { 2, 6, 3, 7 }, // Up
+        { 1, 5, 0, 4 }, // Down
     };
 
-    public static readonly Vector2[] voxelUvs = new Vector2[6] {
-        new Vector2(0.0f, 0.0f), // 0
-        new Vector2(0.0f, 1.0f), // 1
-        new Vector2(1.0f, 0.0f), // 2
-        new Vector2(1.0f, 0.0f), // 3
-        new Vector2(0.0f, 1.0f), // 4
-        new Vector2(1.0f, 1.0f), // 5
+    public static readonly int[] arrayOrder = new int[6] {
+        0, 1, 2, 2, 1, 3
     };
+
+    public static readonly Vector3Int[] inDirection = new Vector3Int[6] {
+        new Vector3Int(0, 0, +1), // North
+        new Vector3Int(0, 0, -1), // South
+
+        new Vector3Int(-1, 0, 0), // West
+        new Vector3Int(+1, 0, 0), // East
+
+        new Vector3Int(0, +1, 0), // Up
+        new Vector3Int(0, -1, 0), // Down
+    };
+}
+
+public enum FaceDirection: int {
+    North = 0,
+    South = 1,
+    West  = 2,
+    East  = 3,
+    Up    = 4,
+    Down  = 5,
+}
+
+public struct VoxelFace {
+    public readonly Vector3Int pos;
+    public readonly FaceDirection dir;
+    public readonly Vector4 uv;
+
+    public VoxelFace(Vector3Int pos, FaceDirection direction, Vector4 uv) {
+        this.pos = pos;
+        this.dir = direction;
+        this.uv = uv;
+    }
+}
+
+public class VoxelMesh {
+    private List<VoxelFace> faces = new List<VoxelFace>();
+
+    public void AddFace(VoxelFace face) {
+        faces.Add(face);
+    }
+    public void AddFace(Vector3Int pos, FaceDirection direction, Vector4 uv) {
+        faces.Add(new VoxelFace(pos, direction, uv));
+    }
+
+    public void ClearMesh() {
+        faces.Clear();
+    }
+
+    public Mesh GenerateMesh() {
+        Vector3[] vertices = new Vector3[faces.Count * 4];
+        Vector2[] uvs = new Vector2[faces.Count * 4];
+        int[] indeces = new int[faces.Count * 6];
+
+        for(int i = 0; i < faces.Count; i++) {
+            VoxelFace face = faces[i];
+            int vertexIndex = i * 4;
+            int triangleIndex = i * 6;
+
+            for(int j = 0; j < 4; j++) {
+                int index = VoxelData.indeces[(int)face.dir, j];
+                vertices[vertexIndex + j] = VoxelData.vertices[index] + face.pos;
+            }
+
+            Vector4 uv = face.uv;
+            uvs[vertexIndex + 0] = new Vector2(uv.x,      uv.y);
+            uvs[vertexIndex + 1] = new Vector2(uv.x,      uv.y+uv.w);
+            uvs[vertexIndex + 2] = new Vector2(uv.x+uv.z, uv.y);
+            uvs[vertexIndex + 3] = new Vector2(uv.x+uv.z, uv.y+uv.w);
+            
+            for(int j = 0; j < 6; j++) {
+                int order = VoxelData.arrayOrder[j];
+                indeces[triangleIndex + j] = vertexIndex + order;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.name = "Chunk Mesh";
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = indeces.ToArray();
+        mesh.uv = uvs.ToArray();
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        return mesh;
+    }
 }
